f9a0fbd8dd553e72ff4efbc61ec2be47ff1952b8
Mahmoud Mustafa Oushi <157726528+Oushim959@users.noreply.github.com>
2025-08-07 09:34:24 +0300

    Update vector_store.py

--- Mahmoud Mustafa Oushi/Q&A_App_FastAPI/vector_store.py
+++ Mahmoud Mustafa Oushi/Q&A_App_FastAPI/vector_store.py
@@ -1,21 +1,3 @@
-# from database import SessionLocal, DocumentChunk
-# from sentence_transformers import SentenceTransformer
-# from sqlalchemy import select, func
-
-# model = SentenceTransformer("all-MiniLM-L6-v2")
-
-# def retrieve_similar_chunks(query: str, top_k: int = 5):
-#     embedding = model.encode([query])[0].tolist()
-#     session = SessionLocal()
-#     stmt = (
-#         select(DocumentChunk)
-#         .order_by(DocumentChunk.embedding.cosine_distance(embedding))
-#         .limit(top_k)
-#     )
-#     results = session.execute(stmt).scalars().all()
-#     session.close()
-#     return results
-
 from sentence_transformers import SentenceTransformer
 from vector_store_sqlite import VectorStoreSQLite  
 
@@ -26,4 +8,5 @@ def retrieve_similar_chunks(query: str, top_k: int = 5):
     embedding = model.encode(query).tolist()
     similar = store.search_similar_chunks(embedding, top_k=top_k)
     return [chunk for score, chunk in similar]
+
 
